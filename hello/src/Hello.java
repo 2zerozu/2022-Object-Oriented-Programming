42fd814e2062697a862069b5be30555971ff5aa3
이빵주 <yungju9982@naver.com>
2022-12-20 03:30:00 +0900

    #1 Lab1

--- hello/src/Hello.java
+++ hello/src/Hello.java
@@ -3,5 +3,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		System.out.println("안녕하세요?");
+		System.out.println("자바를 처음 공부합니다.");
+		System.out.println("자바는 재미있나요?");
 	}
 }
