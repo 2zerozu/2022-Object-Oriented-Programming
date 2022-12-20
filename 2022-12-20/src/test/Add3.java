5d7131fedfd559fc9992e6214aaa368704744993
이빵주 <yungju9982@naver.com>
2022-12-20 10:19:42 +0900

    #2 add3(여러 Scanner 타입 사용) 실습

---
+++ 2022-12-20/src/test/Add3.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.util.Scanner;
+
+public class Add3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String name;
+		int age;
+
+		System.out.print("이름을 입력하세요 : ");
+		name = sc.nextLine();
+
+		System.out.print("나이를 입력하세요 : ");
+		age = sc.nextInt();
+
+		System.out.println(name + "님 안녕하세요! " + age + "살이시네요.");
+
+	}
+
+}
+
+//String (한 단어) -> sc.next();
+//int -> sc.nextInt();
+//double -> sc.nextDouble();
+//String (스페이스바 포함) -> sc.nextLine()
