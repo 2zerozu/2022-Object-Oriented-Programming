977095279adcb9f2a3101521ce8bcf5f455c49e1
이빵주 <yungju9982@naver.com>
2022-12-20 10:29:25 +0900

    #2 자바에서 var 사용 가능

--- 2022-12-20/src/test/TimeCalculator.java
+++ 2022-12-20/src/test/TimeCalculator.java
@@ -6,13 +6,13 @@ public class TimeCalculator {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int time, min, sec;
+//		int time, min, sec;
 
 		System.out.print("초를 입력하세요 : ");
-		time = sc.nextInt();
+		var time = sc.nextInt();
 
-		min = time / 60;
-		sec = time % 60;
+		var min = time / 60;
+		var sec = time % 60;
 
 		System.out.println(time + "초는 " + min + "분 " + sec + "초입니다.");
 	}
