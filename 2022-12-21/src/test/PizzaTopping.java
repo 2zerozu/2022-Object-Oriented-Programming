1aeebb9249febfd0ab61e4eea011b1cf646dcdf9
이빵주 <yungju9982@naver.com>
2022-12-21 10:56:30 +0900

    #3 String 배열 실습

---
+++ 2022-12-21/src/test/PizzaTopping.java
@@ -0,0 +1,13 @@
+package test;
+
+public class PizzaTopping {
+
+	public static void main(String[] args) {
+		String[] toppings = { "Pepperoni", "Mushrooms", "Onions", "Sausage", "Bacon" };
+
+		for (String s : toppings) {
+			System.out.print(s + " ");
+		}
+	}
+
+}
